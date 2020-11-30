bd63c23ef2b459918e191a4eef0085f05e108656
ihorryba <rybaihor@gmail.com>
2020-11-30 14:54:15 +0200

    space

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+
 // app.use(express.static(__dirname + '/dist'));
 
 // app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
